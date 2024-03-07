d0d4619a40d94077eae8f24e1c12bc4f106f3b15
macdiii <50419460+macdiii@users.noreply.github.com>
2024-03-07 11:16:34 +0800

    Delete C2S directory

--- C2S/1-154/subc2s14.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH -p amd_512
-#SBATCH -n 1
-#SBATCH -c 4
-source /public3/soft/modules/module.sh
-module load anaconda/3-Python3.7.4-fenggl
-source activate py38
-python -u 14.py
-
